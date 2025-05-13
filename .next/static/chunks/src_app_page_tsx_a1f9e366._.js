83acba77a6079c1e8140bd4d128c5df9ec007a79
Mhmshn63 <shnali63100@gmail.com>
2025-05-13 22:07:53 +0200

    Add .next/static/chunks/src_app_page_tsx_a1f9e366._.js 1747166873100

---
+++ .next/static/chunks/src_app_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_4898a938._.js",
+  "static/chunks/src_1682ed21._.js"
+],
+    source: "dynamic"
+});
